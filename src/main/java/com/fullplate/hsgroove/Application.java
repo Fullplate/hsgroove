929cd563655f33870f10d510c0765a18e2363311
Fullplate <michaelharmer.au@gmail.com>
2015-03-07 14:05:14 +1100

    Changes for working deploy: - war packaging, scope provided spring-boot-starter-tomcat dependency - standard changes to Application class to deploy to servlet container - production url/username/password for MySQL database

--- src/main/java/com/fullplate/hsgroove/Application.java
+++ src/main/java/com/fullplate/hsgroove/Application.java
@@ -2,14 +2,23 @@ package com.fullplate.hsgroove;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
+    // Used when deploying to a standalone servlet container
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    // Used when launching as an executable jar or war
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
